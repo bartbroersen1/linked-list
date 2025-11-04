28df3e53253d695b25ccd18c4a82b7aac399be82
Bart Broersen <143587354+bartbroersen1@users.noreply.github.com>
2025-11-04 14:54:26 +0100

    initialising classes

--- src/linkedList.js
+++ src/linkedList.js
@@ -1,12 +1,17 @@
 class LinkedList {
-
     constructor () {
 
+    }
+
+    append(value) {
+        if (nextNode === null) {
+            nextNode = new NOde (value, nextNode = null)
+        }
     }
 }
 
 class Node {
-    constructor () {
+    constructor (value = null, nextNode = null) {
         this.value = value;
         this.nextNode = nextNode;
     }
