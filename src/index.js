aed718faf1ae58c1b28fd8a89893b438e7cd6b0f
Bart Broersen <143587354+bartbroersen1@users.noreply.github.com>
2025-11-07 14:45:59 +0100

    coded linkedlist following base exercises

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
-import "./linkedList";
+import {LinkedList} from "./linkedList.js";
+
+const list = new LinkedList();
+
+list.append("dog");
+list.append("cat");
+list.append("parrot");
+list.append("hamster");
+list.append("snake");
+list.append("turtle");
+
+console.log(list.toString());
+
+list.pop();
+list.prepend("chicken");
+
+console.log(list.toString());
--- src/linkedList.js
+++ src/linkedList.js
@@ -1,18 +1,119 @@
 class LinkedList {
     constructor () {
+        this.head = null;
+    }
 
+    head() {
+        return this.head
+    }
+
+    tail() {
+        if (this.head === null) return this.head
+
+        let tmp = this.head;
+        while (tmp.next !== null) {
+            tmp = tmp.next;
+        }
+        return tmp
+    }
+
+    size() {
+        if (this.head === null) return 0
+
+        let size = 1;
+        let tmp = this.head;
+
+        while (tmp.next !== null) {
+            size++;
+            tmp = tmp.next;
+        }
+
+        return size
+    }
+
+    at(index) {
+        if (this.head === null) return new Error (`No item at this index, the list is 0 items long.`);
+
+        let tmp = this.head;
+
+        for (let i = 0; i < index; i++) {
+            if (tmp.next === null) return new Error (`No item at this index, the list is ${i + 1} items long.`);
+            tmp = tmp.next;
+        }
+
+        return tmp
+    }
+
+    contains(value) {
+        if (this.head === null) return false
+        
+        let tmp = this.head;
+
+        while (tmp.value !== value) {
+            if (tmp.next === null) return false
+            tmp = tmp.next;
+        }
+
+        return true
+    }
+
+    find(value) {
+        if (this.head === null) return null
+
+        let index = 0;
+        let tmp = this.head;
+
+        while (tmp.value !== value) {
+            if (tmp.next === null) return null
+            index++;
+            tmp = tmp.next;
+        }
+
+        return index
+    }
+
+    prepend(value) {
+        this.head = new Node (value, this.head)
     }
 
     append(value) {
-        if (nextNode === null) {
-            nextNode = new NOde (value, nextNode = null)
+        if (this.head === null) this.prepend(value);
+        else {
+            this.tail().next = new Node (value);
         }
+    }
+
+    pop() {
+        if (this.head === null) return
+
+        if (this.head.next === null) {
+            this.head = null;
+            return
+        }
+
+        let tmp = this.head;
+        while (tmp.next.next !== null) tmp = tmp.next;
+        tmp.next = null;
+    }
+
+    toString() {
+        let string = "";
+        let tmp = this.head
+
+        while (tmp !== null) {
+            string += `( ${tmp.value} ) -> `;
+            tmp = tmp.next;
+        }
+
+        return string + "null"
     }
 }
 
 class Node {
     constructor (value = null, nextNode = null) {
         this.value = value;
-        this.nextNode = nextNode;
+        this.next = nextNode;
     }
 }
+
+export {LinkedList};
